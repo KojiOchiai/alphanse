944e2d0485f493202ae80cc78cb4872c77197872
KojiOchiai <koji.ochiai@dumy.com>
2017-08-12 18:45:28 +0900

    add new evaluation function to vcca

--- varit/models/vcca.py
+++ varit/models/vcca.py
@@ -70,18 +70,62 @@ class VCCA(chainer.Chain):
                   + rv.gaussian_kl_standard(qzshare)
                   + rv.gaussian_kl_standard(qzy)) / batchsize
         loss = rec_loss + C * kl_loss
-        return loss
+        return loss, qzshare
 
     def bi_lower_bound(self, x, y, weight=0.5, C=1.0, sample=1):
         # loss function
-        lbx = self.lower_bound(x, y, share_source='x',
+        lbx, _ = self.lower_bound(x, y, share_source='x',
                                C=C, sample=sample)
-        lby = self.lower_bound(x, y, share_source='y',
+        lby, _ = self.lower_bound(x, y, share_source='y',
                                C=C, sample=sample)
         loss = weight * lbx + (1 - weight) * lby
         return loss
 
+    def bi_lower_bound_kl(self, x, y, weight=0.5, C=1.0, kl_weight=0.001, sample=1):
+        # loss function
+        lbx, qzgx = self.lower_bound(x, y, share_source='x',
+                                     C=C, sample=sample)
+        lby, qzgy = self.lower_bound(x, y, share_source='y',
+                                     C=C, sample=sample)
+        klzz = rv.gaussian_kl(qzgx, qzgy)
+        loss = weight * lbx + (1 - weight) * lby + kl_weight * klzz
+        return loss
+
 # evaluate
+
+def conditional_log_likelihood_xgx(vae, x, n_sample=1):
+    batchsize = len(x.data)
+    
+    ll_par_x = []
+    for i in range(batchsize):
+        qz = vae.qzgx(x[i:i+1])
+        qzx = vae.qzxgx(x[i:i+1])
+        
+        z_sample = qz.sample(n_sample)
+        zx_sample = qzx.sample(n_sample)
+        px = vae.pxgzxz(F.concat([zx_sample, z_sample]))
+        x_ll = px.log_likelihood(F.tile(x[i:i+1], (n_sample, 1)),
+                                 reduce='no')
+        cll = -np.log(n_sample) + logsumexp(F.sum(x_ll, axis=1).data)
+        ll_par_x.append(cll)
+    return np.mean(ll_par_x)
+
+def conditional_log_likelihood_ygy(vae, y, n_sample=1):
+    batchsize = len(y.data)
+
+    ll_par_y = []
+    for i in range(batchsize):
+        qz = vae.qzgy(y[i:i+1])
+        qzy = vae.qzygy(y[i:i+1])
+        
+        z_sample = qz.sample(n_sample)
+        zy_sample = qzy.sample(n_sample)
+        py = vae.pygzyz(F.concat([zy_sample, z_sample]))
+        y_ll = py.log_likelihood(F.tile(y[i:i+1], (n_sample, 1)),
+                                 reduce='no')
+        cll = -np.log(n_sample) + logsumexp(F.sum(y_ll, axis=1).data)
+        ll_par_y.append(cll)
+    return np.mean(ll_par_y)
 
 def conditional_log_likelihood_xgy(vae, x, y, pzx, n_sample=1):
     batchsize = len(x.data)
