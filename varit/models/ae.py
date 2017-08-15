1792edea4f5427dd352f290753e70041461d51da
KojiOchiai <koji.ochiai@dumy.com>
2017-08-15 22:42:06 +0900

    add autoencoder model

---
+++ varit/models/ae.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import chainer
+import chainer.functions as F
+
+
+class AE(chainer.Chain):
+    def __init__(self, encoder, decoder, loss_func=F.mean_squared_error):
+        super().__init__(
+            encoder=encoder,
+            decoder=decoder,
+        )
+        self.loss_func = loss_func
+    
+    def reconstract(self, x):
+        h = self.encoder(x)
+        x_hat = self.decoder(h)
+        return x_hat
+
+    def loss(self, x, y=None):
+        y = y or x
+        loss = self.loss_func(self.reconstract(x), y)
+        return loss
